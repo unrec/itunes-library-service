fcfe5480d7de62ae55001ccb515d8207b66cf646
unrec <andrei.troitskiy@gmail.com>
2020-10-07 13:51:52 +0300

    library parsing added to start application

--- itunes-library-service-impl/src/main/java/com/unrec/ituneslibrary/ItunesLibraryApplication.java
+++ itunes-library-service-impl/src/main/java/com/unrec/ituneslibrary/ItunesLibraryApplication.java
@@ -2,7 +2,6 @@ package com.unrec.ituneslibrary;
 
 import com.unrec.ituneslibrary.parser.dom.DomParser;
 import lombok.extern.slf4j.Slf4j;
-import org.dom4j.DocumentException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -33,10 +32,10 @@ public class ItunesLibraryApplication {
     @Profile("!test")
     CommandLineRunner commandLineRunner() {
         return args -> {
-            parser.setFile(new ClassPathResource("/library.xml").getInputStream());
             try {
+                parser.setFile(new ClassPathResource("library.xml").getInputStream());
                 parser.parse();
-            } catch (DocumentException e) {
+            } catch (Exception e) {
                 log.warn("Fail to parse a library XML file on start-up: {}", e.getMessage());
             }
             log.info("Library successfully parsed on start-up.");
