f149dc2e016b4703ccc73d1d3f72d18c12bf0915
unrec <andrei.troitskiy@gmail.com>
2020-10-26 20:57:16 +0300

    @NotFound removed due to unnecessary eager fetching

--- itunes-library-service-impl/src/main/java/com/unrec/ituneslibrary/model/Track.java
+++ itunes-library-service-impl/src/main/java/com/unrec/ituneslibrary/model/Track.java
@@ -2,11 +2,10 @@ package com.unrec.ituneslibrary.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.NotFound;
-import org.hibernate.annotations.NotFoundAction;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinColumns;
@@ -33,19 +32,14 @@ public class Track implements Serializable {
     @NotNull
     private String id;
 
-    @ManyToOne
-    @NotFound(action = NotFoundAction.IGNORE)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumns({
-            @JoinColumn(
-                    name = "album_name",
-                    referencedColumnName = "album_name"),
-            @JoinColumn(
-                    name = "album_year",
-                    referencedColumnName = "album_year")
+            @JoinColumn(name = "album_name", referencedColumnName = "album_name"),
+            @JoinColumn(name = "album_year", referencedColumnName = "album_year")
     })
     private Album album;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "artist_name")
     private Artist artist;
 
